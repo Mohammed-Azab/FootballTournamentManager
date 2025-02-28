0f20cbe769ba6fed92147b04cbe2018b5473f9cc
Mohammed Azab <Mo7ammed3zab@outlook.com>
2025-02-28 19:25:23 +0100

    added extra helper methods for the tournament class

--- src/main/java/core/models/Tournament.java
+++ src/main/java/core/models/Tournament.java
@@ -11,9 +11,14 @@ public class Tournament {
     public void addTeam(Team team) {
         teams.add(team);
     }
-
     public void addMatch(Match match) {
         matches.add(match);
+    }
+    public void removeTeam(Team team) {
+        teams.remove(team);
+    }
+    public void removeMatch(Match match) {
+        matches.remove(match);
     }
 
     public void printMatches() {
@@ -21,4 +26,31 @@ public class Tournament {
             System.out.println(match.getResult());
         }
     }
+    public void printTeams() {
+        for (Team team : teams) {
+            System.out.println(team.getName());
+        }
+    }
+    public void printMatchesWithTeams() {
+        for (Match match : matches) {
+            System.out.println(match.getResult());
+            System.out.println(match.getTeam1().getName());
+            System.out.println(match.getTeam2().getName());
+        }
+    }
+    public String toString() {
+        return "Tournament{" +
+                "teams=" + teams +
+                ", matches=" + matches +
+                '}';
+    }
+    public List<Team> getTeams() {
+        return new ArrayList<>(teams);
+    }
+
+    public List<Match> getMatches() {
+        return new ArrayList<>(matches);
+    }
+
+
 }
