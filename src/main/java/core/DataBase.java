e55a8b97c2758f9774792fb0b5468a4580fa73ea
Mohammed Azab <Mo7ammed3zab@outlook.com>
2025-02-28 18:25:48 +0100

    created DataBase class

---
+++ src/main/java/core/DataBase.java
@@ -0,0 +1,37 @@
+package core;
+
+
+import core.models.Match;
+import core.models.Team;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase {
+    private final List<Team> teams = new ArrayList<>();
+    private final List<Match> matches = new ArrayList<>();
+
+    public void addTeam(Team team) {
+        teams.add(team);
+    }
+
+    public List<Team> getTeams() {
+        return new ArrayList<>(teams);
+    }
+
+    public void addMatch(Match match) {
+        matches.add(match);
+    }
+
+    public List<Match> getMatches() {
+        return new ArrayList<>(matches);
+    }
+
+    public void clearTeams() {
+        teams.clear();
+    }
+
+    public void clearMatches() {
+        matches.clear();
+    }
+}
